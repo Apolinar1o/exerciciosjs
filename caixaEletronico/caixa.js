4f82162e78407284e60da3467dac2653b95b9f97
Antônio Apolinário Gonzaga Neto <a.apolinario.neto@gmail.com>
2023-01-25 20:00:32 -0300

    polimento
    nesse commit as alterações foi para o código ficar mais polido e alguns buis foram concertados #1

--- caixaEletronico/caixa.js
+++ caixaEletronico/caixa.js
@@ -17,114 +17,132 @@ function RETIRADA100(nota100, nota50, nota20, nota5, total, nome ) {
     retirada = Number(retirada)
     var conta = retirada/100
     conta = parseInt(conta)  
-    if (retirada > total || retirada % 5 != 0) {
-        console.log("[ERRO] houve um erro")
-        LOOPING(nome, retirada, total, resultado)
-    }
-    else if (retirada % 100 == 0 && conta >= 0 && conta  <= 3)  {
+    var resultado = total-retirada
+    if (retirada > total || retirada % 5 != 0 || retirada < 0) {
+        console.log("[ERRO] DIgite um valor correto")
+        LOOPING(nome, retirada, total, resultado, nota100, nota50, nota20, nota5)
+    } else if (nota100<conta) {
+        console.log("notas de 100 insuficientes")
+        LOOPING(nome, retirada, total, resultado, nota100, nota50, nota20, nota5)
+    }   
+    else if (retirada % 100 == 0 && conta >= 0 && conta  <= nota100)  {
         console.log(`serão ${conta} notas de 100`)
-            console.log("você não pode retirar mais")
-            console.log("REINICIANDO...")
+        console.log("e agora você tem " + (resultado +" reais"))
+        nota100 = nota100-resultado
+        LOOPING(nome, retirada, total, resultado, nota100, nota50, nota20, nota5)
+            
     }
     else if (retirada > (100 * nota100)){
         console.log(`serão ${nota100} notas de 100`)
         conta = retirada - (100 * nota100)
-        RETIRADA50(nota50, nota20, nota5, retirada, total, conta, nome)
+        nota100 = 0
+        RETIRADA50(nota50, nota20, nota5, retirada, total, conta, nome, resultado)
     }  else {
         console.log(`serão ${conta} notas de 100`)
         conta = retirada - (conta*100)
-        RETIRADA50(nota50, nota20, nota5, retirada, total, conta, nome )
+        nota100 = nota100-conta
+        RETIRADA50(nota50, nota20, nota5, retirada, total, conta, nome, resultado )
         
     }
 }
 // quantidade de notas de 50
-function RETIRADA50(nota50, nota20, nota5, retirada, total, conta, nome  ) {
+function RETIRADA50(nota50, nota20, nota5, retirada, total, conta, nome, resultado  ) {
     qtd50 = conta
     conta = qtd50/50
     conta = parseInt(conta)
-    
-    if (qtd50 % 50 == 0 && conta >=0 && conta<=2) {
+    if (nota50 < conta) {
+        console.log("notas de 50 insuficientes")
+        LOOPING(nome, retirada, total, resultado, nota100, nota50, nota20, nota5)
+   
+    } else if (qtd50 % 50 == 0 && conta >=0 && conta<=nota50) {
         console.log(`serão ${conta} notas de 50`)
-        var resultado = total-retirada
         console.log("e agora você tem " + (resultado +" reais"))
-        if (resultado == 0) {
-            console.log("você não pode retirar mais")
-            console.log("REINICIANDO...")
-        } else {
-            LOOPING(nome, retirada, total, resultado)
-        }
-    } else if (qtd50 > ( nota50*50)) {
+        nota50 = nota50-conta
+        LOOPING(nome, retirada, total, resultado, nota100, nota50, nota20, nota5)
+
+     }  else if (qtd50 > ( nota50*50)) {
         console.log(`serão ${nota50} notas de 50`)
         conta = qtd50 - (nota50*50)
-        RETIRADA20(nota20, nota5, retirada, total, conta, nome )
-    } else {
+        nota50 = 0
+        RETIRADA20(nota20, nota5, retirada, total, conta, nome , resultado)
+     } else {
         console.log(`serão ${conta} notas de 50`)
         conta = qtd50 - (conta*50)
-        RETIRADA20(nota20, nota5, retirada, total, conta, nome )
+        nota50 = nota50-conta
+        RETIRADA20(nota20, nota5, retirada, total, conta, nome, resultado )
     }
+   
 }
+
 // quantidade de notas de 20
-function RETIRADA20( nota20, nota5, retirada, total, conta , nome ) {
+function RETIRADA20( nota20, nota5, retirada, total, conta , nome , resultado) {
     qtd20 = conta
     conta = qtd20/20
     conta = parseInt(conta)
-    
-    if (qtd20 % 20 == 0 && conta >=0 && conta<=4) {
+    if (nota20< conta) {
+        console.log("notas de 20 insuficientes")
+        LOOPING(nome, retirada, total, resultado, nota100, nota50, nota20, nota5)
+    }
+    else if (qtd20 % 20 == 0 && conta >=0 && conta<=nota20) {
         console.log(`serão ${conta} notas de 20`)
         var resultado = total-retirada
         console.log("e agora você tem " + (resultado +" reais"))
+        nota20 = nota20 - conta
         if (resultado == 0) {
             console.log("você não pode retirar mais")
             console.log("REINICIANDO...")
         } else {
-            LOOPING(nome, retirada, total, resultado)
+            LOOPING(nome, retirada, total, resultado, nota100, nota50, nota20, nota5)
         }
     } else if (qtd20 > ( nota20*20)) {
         console.log(`serão ${nota20} notas de 20`)
         conta = qtd20 - (nota20*20)
-        RETIRADA5( nota5, retirada, total, conta, nome )
+        nota20 = 0
+        RETIRADA5( nota5, retirada, total, conta, nome, resultado )
     } else {
         console.log(`serão ${conta} notas de 20`)
         conta = qtd20 - (conta*20)
-        RETIRADA5( nota5, retirada, total, conta, nome )
+        nota20 = nota20 - conta
+        RETIRADA5( nota5, retirada, total, conta, nome, resultado )
     }
 }
 // quantidade de notas de 5
-function RETIRADA5( nota5, retirada, total, conta, nome ) {
+function RETIRADA5( nota5, retirada, total, conta, nome , resultado) {
     qtd5 = conta
     conta = qtd5/5
     conta = parseInt(conta)
-    
-    if (qtd5 % 5 == 0 && conta >=0 && conta<=4) {
-        console.log(`serão ${conta} notas de 5`)
-        var resultado = total-retirada
-        console.log("e agora você tem " + (resultado +" reais"))
-        if (resultado == 0) {
+    console.log(`serão ${conta} notas de 5`)
+    console.log("e agora você tem " + (resultado +" reais"))
+    nota5 = nota5 -conta
+        if (nota5 < conta) {
+            console.log("notas de 5 insufiencientes")
+            LOOPING(nome, retirada, total, resultado, nota100, nota50, nota20, nota5)
+        } 
+        else if (resultado == 0) {
             console.log("você não pode retirar mais")
             console.log("REINICIANDO...")  
         } else {
-            LOOPING(nome, retirada, total, resultado)
+            LOOPING(nome, retirada, total, resultado, nota100, nota50, nota20, nota5)
         }
      
-    } 
+    
 }
 // função para loopar até o usuario querer ou ter saldo
-function LOOPING(nome, retirada, total, resultado) {
+function LOOPING(nome, retirada, total, resultado, nota100, nota50, nota20, nota5) {
     
-     let confirm = entrada("Você quer continuar?(sim ou nao) ")
-        if (confirm == "nao") {
+     var confirm = entrada("Você quer continuar?(sim ou nao) ")
+        if (confirm != "sim") {
             console.log("não quer continuar tudo bem!!")
-        }
-        else if (retirada <= total && confirm == "sim") {
+                
+        } else if (retirada <= total && confirm == "sim") {
     
         while(retirada <= total) {
             total = resultado
-            console.log("-----------------------------------------------------")
-            RETIRADA100(nota100, nota50, nota20, nota5, total, nome)      
-           
-            
-    }
-    } else {
+            console.log("-------------------------------------------------")
+            RETIRADA100(nota100, nota50, nota20, nota5, total, nome)  
+            break    
+        }
+        }  else {
         console.log("você não pode retirar mais")
         console.log("adeus " + nome )
  }
